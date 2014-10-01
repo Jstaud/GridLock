4ead5c00cdaa4c391b4ec0f157c4a79c6b7d4375
Jonny Tran <nhat.tran@mavs.uta.edu>
2014-10-01 17:32:44 -0500

    Added location, speed & direction update on interval

--- GridLockIonic/www/js/app.js
+++ GridLockIonic/www/js/app.js
@@ -8,32 +8,84 @@ angular.module('starter', ['ionic'])
 
 
 .run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if (window.StatusBar) {
-            StatusBar.styleDefault();
-        }
-
-
-
-    });
-
-
+  $ionicPlatform.ready(function () {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
 })
 
 
+.controller('locationCtrl', function($scope, $ionicLoading, $interval) {
 
-.controller('test', function($scope) {
-  $scope.data = [
-    { goalSpeed: '55',
-     speedLimit: '60',
-     units: 'MPH'
-    }
-  ];
+  function initialize() {
+
+    $scope.data = [
+      { 
+        goalSpeed: 55,
+        speedLimit: 60,
+        currentSpeed: 0,
+        units: 'MPH',
+        lat: 0,
+        lng: 0,
+        direction: ''
+      }
+    ];
+
+    // var mapOptions = {
+    //   center: myLatlng,
+    //   zoom: 16,
+    //   mapTypeId: google.maps.MapTypeId.ROADMAP
+    // };
+
+    // var map = new google.maps.Map(mapOptions);
+
+    // $scope.map = map;
+  }
+  
+  $scope.getCurrentPosition = function(gotPosition, gotSpeed, gotDirection) {
+
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      if ($scope.loading) {
+        $scope.loading.hide();
+      };
+
+      gotPosition(pos.coords.latitude, pos.coords.longitude);
+      gotSpeed(pos.coords.speed);
+      gotDirection(pos.coords.heading);
+    }, 
+    function(error) {
+      $scope.loading = $ionicLoading.show({
+        template: 'Cannot get current location. \nRetrying...',
+        showBackdrop: true
+      });
+    });
+  };
+
+  
+  window.addEventListener('load', initialize);
+  
+  $interval(function() {
+    $scope.getCurrentPosition(function(lat, lng) {
+      console.log(lat + ", " + lng + ", " );
+      $scope.data[0]['lat'] = lat;
+      $scope.data[0]['lng'] = lng;
+    },
+    function(speed) {
+      console.log("speed: ", speed);
+      $scope.data[0]['speedLimit'] = speed ? speed : "no speed";
+    },
+    function(direction) {
+      console.log("direction: ", direction);
+    });
+
+  }, 2000);
+
 });
 
 
